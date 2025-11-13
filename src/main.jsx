283752bc9590c228b98198a447f3e146721ed4ea
JACK- <jack17@ghostlayer.ai>
2025-11-13 11:12:42 -0800

    Final mythic deploy of awareness academy JSX

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import { MoktarAIProvider } from './context/MoktarAIContext.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MoktarAIProvider>
-      <App />
-    </MoktarAIProvider>
+    <App />
   </React.StrictMode>
-);
+)
